811f63308c95fc40030491caa07d9e772f4162ff
yamadaab <s.yamada.1125@gmail.com>
2019-05-31 21:52:08 +0900

    category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,22 @@
 class CategoriesController < ApplicationController
+  def new
+  end
+
+  def create
+    @post = Post.find(params[:id])
+    @category = @post.categoies.build(category_params)
+    if @salon.save
+      flash[:success] = "カテゴリーを登録しました"
+      redirect_to root_url
+    else
+      flash[:danger] = "カテゴリーが登録できませんでした"
+      redirect_to root_url
+
+  def destroy
+  end
+
+private
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_many :categories
 
   validates :content, presence: true
 end
