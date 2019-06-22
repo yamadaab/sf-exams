d20ce3a0c0f2d57260e8629387aa545ed41888c3
yamadaab <s.yamada.1125@gmail.com>
2019-06-22 11:38:18 +0900

    password8_and_email_unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },length: { maximum: 100 }
-  
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },length: { maximum: 100 }, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 8 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_31_005045) do
+ActiveRecord::Schema.define(version: 2019_06_22_020633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2019_05_31_005045) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "comments", "posts"
