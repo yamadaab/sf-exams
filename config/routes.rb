3374b7e75bfea3e72723276f073bf2ea7ea49acb
yamadaab <s.yamada.1125@gmail.com>
2019-06-22 10:18:29 +0900

    route_sessions#new

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   root to: 'static_pages#home'
   get 'home', to: "static_pages#home"
   get 'about', to: "static_pages#about"
-  get 'login', to: "session#new"
+  get 'login', to: "sessions#new"
   post 'login', to: "sessions#create"
   get 'logout', to: "sessions#destroy"
   resources :users
