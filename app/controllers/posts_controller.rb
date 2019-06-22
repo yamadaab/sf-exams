0ce56096f11b4cdb5bd1428ec13bfbf905a8388c
yamadaab <s.yamada.1125@gmail.com>
2019-06-22 10:30:31 +0900

    post_params_add_:content

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -54,7 +54,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title)
+    params.require(:post).permit(:title, :content)
   end
 
   def correct_user?
